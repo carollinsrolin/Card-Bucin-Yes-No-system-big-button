127e388d8e4913f5acf88148bd285f032f4b0d09
carollins <128696308+carollinsrolin@users.noreply.github.com>
2024-03-19 23:46:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const buttonNo = document.querySelector('#no')
+const buttonYes = document.querySelector('#yes')
+
+let fontSize = 2
+
+let messages = [
+  'Yakin ?',
+  'Pikirkan agik nanti nyesal lo',
+  'Ayolahhh',
+  'Pleasee',
+  'Yakin deh pasti seru',
+  'Mau yaaa'
+]
+
+buttonNo.addEventListener('click', () => {
+  fontSize = fontSize + .5
+  buttonYes.style.fontSize = `${fontSize}rem`
+
+  const indexRandom = Math.floor(Math.random() * messages.length)
+
+  buttonNo.textContent = messages[indexRandom]
+})
+
+buttonYes.addEventListener('click', () => {
+  document.querySelector('#message').style.display = 'flex'
+})
